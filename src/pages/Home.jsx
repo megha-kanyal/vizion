4d645d0c7f235c66d39331d8e07a31cbe7727105
Chirag1724 <chiragdwivediji@gmail.com>
2025-03-06 18:42:39 +0530

    Profile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import Hero from '../Components/Hero'
 
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <Navbar/>
-      Home
+      <Footer/>
 
     </div>
   )
