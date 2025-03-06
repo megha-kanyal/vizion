f17a1da6b324f44098f3a8cc7800baa5dd6ac149
Megha <meghakanyal395@gmail.com>
2025-03-07 01:26:40 +0530

    home updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Events from './pages/Events';
 import Internship from './pages/Internship';
@@ -16,15 +15,12 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/events" element={<Events />} />
         <Route path="/internship" element={<Internship />} />
         <Route path="/alumini" element={<Alumini />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/techtalks" element={<Techtalks/>} />
-
-
         <Route path='/addevents' element={<AddEvents/>}/> 
         <Route path='/CreateJobs' element={<CreateJobs/>}/>
       </Routes>
