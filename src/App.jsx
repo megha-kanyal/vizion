479663ecf5188f75991396ea23aa5073c4b935b6
Soumya Bhakuni <soumyabhakuni456@gmail.com>
2025-03-06 22:56:59 +0530

    Addes Techtalks.jsx final version

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Internship from './pages/Internship';
 import Alumini from './pages/Alumini';
 import Profile from './pages/Profile';
 import Techtalks from './pages/Techtalks';
-import Mentorship from './pages/Mentorship';
 import AddEvents from './pages/AddEvents';
 
 function App() {
@@ -22,8 +21,7 @@ function App() {
         <Route path="/internship" element={<Internship />} />
         <Route path="/alumini" element={<Alumini />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/tecktalks" element={<Techtalks/>} />
-        <Route path="/mentorship" element={<Mentorship/>} />
+        <Route path="/techtalks" element={<Techtalks/>} />
         <Route path='/addevents' element={<AddEvents/>}/>
       </Routes>
     </Router>
